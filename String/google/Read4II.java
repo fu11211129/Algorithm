e504d0f995fbe86c294bd44037d429ba6bb53247
Liangjia Fu <fuliangjia1129@gmail.com>
2018-06-13 11:33:49 -0700

    u

---
+++ String/google/Read4II.java
@@ -0,0 +1,19 @@
+public class Read4II {
+    private int buffPtr = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            if (buffPtr == 0) {
+                buffCnt = read4(buff);
+            }
+            if (buffCnt == 0) break;
+            while (ptr < n && buffPtr < buffCnt) {
+                buf[ptr++] = buff[buffPtr++];
+            }
+            if (buffPtr == buffCnt) buffPtr = 0;
+        }
+        return ptr;
+    }
+}
